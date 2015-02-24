a3928c73ffc3047446b06eca5631bfa71ac8eb18
driveunit <driveunit@gmail.com>
2015-02-24 15:10:14 -0800

    Account_looper
    Added the Account_looper.java class

---
+++ src/testCases/Accounts_looper.java
@@ -0,0 +1,19 @@
+package testCases;
+
+import java.util.ArrayList;
+
+
+
+public class Accounts_looper {
+
+	private boolean login_successfull;
+	
+	public void account_looper(MVPD mvpd){	
+		ArrayList<Accounts> accountLists = mvpd.getAccount();
+		for(Accounts account : accountLists){
+			
+				
+		}
+				
+	}
+}
