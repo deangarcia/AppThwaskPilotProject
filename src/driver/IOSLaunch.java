3768798d6506a3454b20a4f8788558bb7b7f5618
Dean <dean.garcia@csulb.edu>
2015-03-26 17:02:37 -0700

    most updated

--- src/driver/IOSLaunch.java
+++ src/driver/IOSLaunch.java
@@ -2,15 +2,13 @@ package driver;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.ios.IOSDriver;
-
+import io.appium.java_client.remote.MobileCapabilityType;
 import java.net.URL;
 import java.util.ArrayList;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import testCases.MVPD;
 import testCases.jsonRead;
 
@@ -24,18 +22,39 @@ public abstract class IOSLaunch {
 	public static void launchDriver() throws Throwable 
 	{
 		jsonReader.readJsonFromFile();
+		
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("deviceName", "Raj Bangaru Samy’s iPhone");
-		capabilities.setCapability("platformName", "ios"); 
-		capabilities.setCapability("bundleId", "com.uie.foxsports.foxsportsgo");
+		// Anrdoid
+		/*
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.0.4");
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy S3");
+		capabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "");//appkey
+		// capabilities.setCapability(MobileCapabilityType.APP, myApp);//file id
+		*/
+		
+		
+		// AppthWack 
+		capabilities.setCapability("deviceName", "Apple iPhone 5"); // name of the device as listed on the appthwack api
+		capabilities.setCapability("platformVersion", "7.0"); // os of the device
+		capabilities.setCapability("automationName", "appium"); 
+		capabilities.setCapability("app", "206621"); // change this for every apk or ipa that you upload to appthwack
+		capabilities.setCapability("apiKey", "dbd3ee8964e351efc4017617921094461944a118"); // this is a unique key tied to a specific user
+		capabilities.setCapability("project", "40241"); // project key is shown in the project setting in your account
+		
+		// IOS
 		//iphone4s
-		capabilities.setCapability("platformVersion", "7.0");
-		capabilities.setCapability("udid", "aa18c45a49bcea5055be2e894748ccb25665e1c6");
+		//capabilities.setCapability("platformVersion", "7.0");
+		//capabilities.setCapability("udid", "aa18c45a49bcea5055be2e894748ccb25665e1c6");
+		//capabilities.setCapability("bundleId", "com.uie.foxsports.foxsportsgo");
 		//iphone5
-		//capabilities.setCapability("udid", "ce7d4a568c96f9886ed561d6aad36007e13fe0ff");
-		//capabilities.setCapability("platformVersion", "7.1");
-		driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		wait = new WebDriverWait(driver, 60);
+		//apabilities.setCapability("udid", "ce7d4a568c96f9886ed561d6aad36007e13fe0ff");
+		
+		//appium server
+		//driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		
+		//appthwack server
+		driver = new IOSDriver(new URL("https://appthwack.com/wd/hub"), capabilities);
 	}
 	
 	// Everything in the method annotated with the AfterClass occurs at the very end of every junit test
--- src/testCases/AAA.java
+++ src/testCases/AAA.java
@@ -1,183 +1,201 @@
 package testCases;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
-import org.openqa.selenium.NoSuchElementException;
-
-import java.util.concurrent.TimeUnit;
-
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import driver.IOSLaunch;
 
 public class AAA extends IOSLaunch {
 
+	private String video_xpath = "//UIAApplication[1]/UIAWindow[1]/UIACollectionView[2]/"
+			+ "UIACollectionCell[1]/UIACollectionView[1]/UIACollectionCell";
+	private WebElement ele_ = driver.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAStaticText[1]"));
+	
+	private WebElement waitForElementXpath(String xpath,int waitTime) {
+		wait = new WebDriverWait(driver, waitTime);
+		WebElement element = wait
+				.until(ExpectedConditions.elementToBeClickable(By
+						.xpath(xpath)));
+		return element;
+		
+	}
+	
+	private WebElement waitForElementName(String name,int waitTime) {
+		
+		wait = new WebDriverWait(driver, waitTime);
+		WebElement element = wait
+				.until(ExpectedConditions.elementToBeClickable(By
+						.name(name)));
+		return element;
+	}
 	public static void tap(WebElement element) {
 		driver.tap(1, element, 1);
-	}
-
-	public static WebElement waitForElement_xpath(String xpath) throws TimeoutException {
-		WebElement element = wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath(xpath)));
-
-		return element;
 	}
 
 	public static void click(WebElement element) {
 		element.click();
 	}
-
-	public static WebElement waitForElement_name(String name) throws TimeoutException {
-		WebElement element = wait.until(ExpectedConditions
-				.elementToBeClickable(By.name(name)));
-		return element;
-	}
-	
-	public static void settings() {
-		// settings
-		tap(waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[4]"));
-		// sign in
-		tap(waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[1]"));
-	
-		// implicit wait for 10 seconds driver.swip will wait 10 seconds before 
-		// executing the swipe this ensure that the page is loaded and the 
-		// swipe occurs at the right time
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.findElementByName("Sign in with your TV provider to watch now.");
-		// Swipe to get more providers
-		driver.swipe(290, 420, 290, 250, 3000);
-		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+	private void  takescreenshots(String xpath) throws InterruptedException {
 		
-		tap(waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIACollectionView[1]/UIAButton[2]"));
-	}
+		//tap(waitForElementXpath(xpath,60));
+		driver = (IOSDriver) new Augmenter().augment(driver);
+		Thread.sleep(9000);
+		// Get the screenshot
+		File scrFile = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
+		System.out.println("Screenshot completed");
+		try{
 
-	public static boolean login(String prov, String username, String password) {
-		WebElement e = driver.findElementByName(prov);
+			File calsspathRoot = new File(System.getProperty("user.dir")); 
+			//workspace space is set in application folder
+			
+			File testScreenShot = new File(calsspathRoot + "screenShots", "preRollAds");
+			// Copy the file to screenshot folder
+			FileUtils.copyFile(scrFile, testScreenShot);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void pressButton(String name){
+		while (!(ele_.isDisplayed())) {
+			driver.tap(1, 290, 210, 1);
+			tap( waitForElementName(name,15));	
+			break;
+		}
+		
+	}
+	public void baby(String accountName) {
+		
+		tap(waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/"
+				+ "UIANavigationBar[1]/UIAButton[4]",60));
+	}
+	public void loginAccountSettings (String accountName) {
+		
+		tap(waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/"
+				+ "UIANavigationBar[1]/UIAButton[4]",60));
+		//sign in
+		tap (waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/"
+				+ "UIATableCell[1]/UIAStaticText[1]",60));
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//driver.swipe(225,500,225,250,3000);
+		driver.swipe(290, 420, 290, 234, 3000);
+		//more providers
+		tap(waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/UIACollectionView[1]/"
+				+ "UIAButton[2]",60));
+		//at&t
+		WebElement e = driver.findElementByName(accountName);
 
 		// Scroll to works when invisible elements are present in the DOM
 		// We know that elements are present on the DOM if they have a y coordinate 
 		// Location, 
-		driver.scrollTo(prov);
-		
-		/*
-		while (y > 420) {
-			// Swipe to the location of the MVPD
-			driver.swipe(290, 420, 290, 0, 3000);
-			y = y - 420;
-		}
-		*/
-		tap(e);
-
-		// xpaths are static right now and it works for these two providers but
-		// it does need to be dynamic because they are not all the same
-		// username
-		WebElement element = waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIATextField[1]");
-		if (!(element.getAttribute("value").equals(username))) {
-			click(element);
-			element.sendKeys(username);
-			tap(waitForElement_xpath("//UIAApplication[1]/UIAWindow[2]/UIAToolbar[1]/UIAButton[3]"));
-		}
-
-		// password
-		//element = waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIASecureTextField[1]");
-		element = waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIATextField[2]");
-		click(element);
-		element.sendKeys(password);
-
-		// done
-		tap(waitForElement_xpath("//UIAApplication[1]/UIAWindow[2]/UIAToolbar[1]/UIAButton[3]"));
-
-		// click Sign-In
-		tap(waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIAButton[1]"));
-		
-		// possible sleep here bc elements are crashing
-		return true;
+		driver.scrollTo(accountName);
+		tap(waitForElementName(accountName,60));
 	}
-/*
-// code for element location search
-	public static boolean playVideo(String xpath) throws InterruptedException {
-		List<WebElement> elements;
-		elements = driver.findElementsByXPath(xpath);
-		System.out.println("first find");
-		System.out.println(elements.size() + " initial element size");
-		while (elements.size() == 0) {
-			System.out.println(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
-			elements = driver.findElementsByXPath(xpath);
-			System.out.println(elements.size() + " element size");
-		}
-		List<String> cell_names = new ArrayList<String>();
-
-        for (WebElement cell : elements) {
-            cell_names.add(cell.getAttribute("name"));
-            System.out.println(cell.getAttribute("name"));
-        }
-		// gives the div exRet UIAStatcText
-		// System.out.println(elements.get(elements.size() - 1).getTagName());
-		// System.out.println(elements.get(elements.size() -
-		// 1).getAttribute("name"));
-		// can use to see if WebElement or Native
-		// System.out.println(elements.get(elements.size() - 1).getClass());
-		// dimension could use for swipe left and right
-		// System.out.println(elements.get(elements.size() - 1).getSize());
-		// System.out.println(elements.get(elements.size() - 1).getText());
-		// useless so far System.out.println(elements.get(elements.size() -
-		// 1).isEnabled());
-		// System.out.println(elements.get(elements.size() - 1).isDisplayed());
-        
-		//System.out.println(elements.get(elements.size() - 1).toString());
-		
-        int y = elements.get(elements.size() - 1).getLocation().getY();
-		System.out.println(y + " y value");
-		System.out.println("location find");
-
-		while (!(elements.get(elements.size() - 1).isDisplayed())) {
-			if (y > 420) {
-				if ((y - 420) < 100) {
-					driver.swipe(290, 420, 290, 100, 3000);
-					y = y - 320;
-				} else {
-					driver.swipe(290, 420, 290, 0, 3000);
-					y = y - 420;
-				}
-			} else {
-				driver.swipe(290, 420, 290, y, 3000);
-				elements.add(elements.size() - 1, driver.findElement(By.name(elements.get(elements.size() - 1).getAttribute(
-						"name"))));
-			}
-			Thread.sleep(1000);
-			System.out.println(!(elements.get(elements.size() - 1).isDisplayed()));
-		}
 	
-
-		System.out.println("location find end");
-		System.out.println("tap");
-		System.out.println(elements.get(elements.size() - 1).getAttribute(
-				"name"));
-		tap(waitForElement_name(elements.get(elements.size() - 1).getAttribute(
-				"name")));
-		System.out.println("tap end");
-
-		// if true is returned here that means that the video we tried to click
-		// is not on now
-		System.out.println("is vid displayed");
-		if ((elements.get(elements.size() - 1).isDisplayed())) {
-			System.out.println("unplayable video");
+	public boolean login(Accounts account) {
+		//username
+		WebElement element = waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/"
+				+ "UIAScrollView[1]/UIAWebView[1]/UIATextField[1]",60);
+		click(element);
+		element.sendKeys(account.getUsername());
+		
+		//password
+		element = waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/"
+				+ "UIAWebView[1]/UIATextField[2]",60);
+		click(element);
+		element.sendKeys(account.getPassword());
+		
+		//done
+		tap(waitForElementXpath("//UIAApplication[1]/UIAWindow[2]/UIAToolbar[1]/UIAButton[3]",60));
+		
+		//click Sign-In	
+		tap(waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/"
+				+ "UIAWebView[1]/UIAButton[1]",60));
+		
+		
+		try{
+			  Thread.sleep(1000);
+			  //driver.swipe(225,500,225,250,3000);
+			  driver.swipe(290, 420, 290, 234, 3000);
+			  waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/"
+			  		+ "UIAWebView[1]/UIAButton[1]",15).isEnabled();
+		} catch (Exception e) {
 			return true;
 		}
-		System.out.println("is vid displayed end");
-
+		
 		return false;
+		
 	}
-*/
 	
+	public void playVideos() throws InterruptedException {
+		String xpath="";
+		Thread.sleep(5000);
+		int i=1;
+		List<WebElement> labels= driver.findElementsByXPath("//UIAApplication[1]/UIAWindow[1]/"
+				+ "UIACollectionView[2]/UIACollectionCell[1]/UIACollectionView[1]/UIACollectionCell");
+		System.out.println("Size:" + labels.size());
+		
+		while(i>0 && i<= labels.size()) {
+			Thread.sleep(5000);
+			System.out.println("Iteration:" + i);
+			xpath = video_xpath+"["+i+"]"+"/UIAStaticText[1]";
+			System.out.println("XPATH: " +xpath);
+			WebElement ele = driver.findElementByXPath(xpath);
+			String name = ele.getText();
+			
+			if(!name.equalsIgnoreCase("ON NOW")) {
+				break;
+			
+			}
+			//xpath = video_xpath+"["+i+"]"+"/UIAStaticText[3]";
+			//name = driver.findElementByXPath(xpath).getText();
+			tap(ele);
+			takescreenshots(xpath);
+			Thread.sleep(9000);
+			//pressButton("btn play");
+			pressButton("iPhone video backarrow");	
+			Thread.sleep(3000);	
+			System.out.println("SWIPE");
+			int j=0;
+			while(j<i){
+				//driver.swipe(225,440,225,250,1000);
+				driver.swipe(290, 420, 290, 234, 1000);
+				j++;
+			}
+			i++;
+		}
+		
+		
+	}
+	
+	public void logout(){
+		tap(waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[4]",60));
+		tap(waitForElementXpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/"
+				+ "UIATableCell[1]/UIAStaticText[1]",60));
+		tap(waitForElementName("OK",60));
+	}
+	
+	/*
 	// Same code as above but different approach in vid is the length of a list that contains elements that have 
 	// The value ONNOW in them this means the amount of playable videos and the code manually scrolls through that 
 	// Many times since each video container is 186 pixels from top to bottom swipe down that far then click in the middle 
@@ -193,19 +211,5 @@ public class AAA extends IOSLaunch {
 		driver.tap(1, 195, 290, 1);
 		// Swipe to get more providers
 	}
-	
-	public static void backbutton(String name) throws TimeoutException {
-		tap(waitForElement_name(name));
-	}
-
-	public static void logout() {
-		// settings
-		tap(waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[4]"));
-		tap(waitForElement_xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[1]"));
-		tap(waitForElement_name("OK"));
-	}
-	public static void pauseVideo(String name) throws TimeoutException {
-				tap(waitForElement_name(name));
-	}
-
+	*/
 }
